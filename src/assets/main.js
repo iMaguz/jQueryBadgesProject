709bccdb6d67206f0e255ad9cd37c1ea174912c9
iMaguz <imaguz+github@hotmail.com>
2016-12-16 16:13:20 -0500

    Use jQuery to Fetch and Show Code School Badges Using Ajax

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,14 @@
-$(function() {
-
-  // your code will go here
-
+$.ajax({
+    url: 'https://www.codeschool.com/users/imaguz.json',
+    dataType: 'jsonp',
+    success: function(response) {
+    var badgeElements = $.map(response.courses.completed, function (badge, index){
+        var badgeDiv = $('<div class="course"></div>');
+        $('<h3>'+badge.title+'</h3>').appendTo(badgeDiv);
+        $('<img src="'+badge.badge+'">').appendTo(badgeDiv);
+        $('<a href="'+badge.url+'" target="_blank" class="btn btn-primary">See Course</a>').appendTo(badgeDiv);
+        return badgeDiv;
+    });
+    $('#badges').html(badgeElements);
+    }
 });
